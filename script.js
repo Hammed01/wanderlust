fe5f5952ef38c0a6c98d92cbf5986d9cadc662df
Hammed01 <shotolahammed01@gmail.com>
2018-08-16 15:49:21 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.mobile-nav').click(function(){
+    let nav = $('.right');
+    let icon = $('.mobile-nav i');
+    nav.slideToggle(200);
+    
+    });
+    
+    });
